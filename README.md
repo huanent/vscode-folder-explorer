12a49f0a0bf1657c880dd7e84490a1144f8f213c
huanent <huanent@icloud.com>
2026-07-15 00:40:41 +0800

    init

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
-# Node.js Script Runner
-
-One click run or debug Node.js script.
-
-## Features
-
-* Support typescript
-* Support run on untitled file
-* Run or debug from editor navigation bar
-* Run or debug from explorer context menu
-
-![](resources/preview.png)
+# TODO
--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,7 @@
 import * as vscode from 'vscode';
-import { registerRunScriptCommand } from './commands/run-script';
-import { registerDebugScriptCommand } from './commands/debug-script';
 
 export function activate(context: vscode.ExtensionContext) {
-	registerRunScriptCommand(context);
-	registerDebugScriptCommand(context);
+	
 }
 
 export function deactivate() { }
